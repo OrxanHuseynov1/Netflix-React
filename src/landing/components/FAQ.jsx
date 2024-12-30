167c9be674094f210b8c12b3b42c600f66b51444
Orxan Huseynov <115917125+OrxanHuseynov1@users.noreply.github.com>
2024-12-30 23:00:31 +0400

    All

--- src/landing/components/FAQ.jsx
+++ src/landing/components/FAQ.jsx
@@ -1,43 +1,46 @@
 import {useState} from 'react'
 import FAQItem from './FAQItem'
 import EmailInput from './EmailInput'
+import { useTranslation } from 'react-i18next';
+
 const FAQ = () => {
+    const { t } = useTranslation(); 
     const [openItem,setOpenItem] = useState(null)
     
     const items = [
         {
-            title:"What is Netflix?",
-            desc:"Netflix is a streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries, and more on thousands of internet-connected devices.[break]You can watch as much as you want, whenever you want without a single commercial – all for one low monthly price. There's always something new to discover and new TV shows and movies are added every week!",
+            title:t('what_is_netflix'),
+            desc:t('what_is_netflix_answer'),
             id:1
         },
         {
-            title:"How much does Netflix cost?",
-            desc:"Watch Netflix on your smartphone, tablet, Smart TV, laptop, or streaming device, all for one fixed monthly fee. Plans range from EUR 7.99 to EUR 11.99 a month (pre-tax). No extra costs, no contracts.",
+            title:t('how_much_does_netflix_cost'),
+            desc:t('how_much_does_netflix_cost_answer'),
             id:2
         },
         {
-            title:"Where can I watch?",
-            desc:"Watch anywhere, anytime. Sign in with your Netflix account to watch instantly on the web at netflix.com from your personal computer or on any internet-connected device that offers the Netflix app, including smart TVs, smartphones, tablets, streaming media players and game consoles.[break]You can also download your favorite shows with the iOS or Android app. Use downloads to watch while you're on the go and without an internet connection. Take Netflix with you anywhere.",
+            title:t('where_can_i_watch'),
+            desc:t('where_can_i_watch_answer'),
             id:3
         },
         {
-            title:"How do I cancel?",
-            desc:"Netflix is flexible. There are no pesky contracts and no commitments. You can easily cancel your account online in two clicks. There are no cancellation fees – start or stop your account anytime.",
+            title:t('how_do_i_cancel'),
+            desc:t('how_do_i_cancel_answer'),
             id:4
         },
         {
-            title:"What can I watch on Netflix?",
-            desc:"Netflix has an extensive library of feature films, documentaries, TV shows, anime, award-winning Netflix originals, and more. Watch as much as you want, anytime you want.",
+            title:t('what_can_i_watch_on_netflix'),
+            desc:t('what_can_i_watch_on_netflix_answer'),
             id:5
         },
         {
-            title:"Is Netflix good for kids?",
-            desc:"The Netflix Kids experience is included in your membership to give parents control while kids enjoy family-friendly TV shows and movies in their own space.[break]Kids profiles come with PIN-protected parental controls that let you restrict the maturity rating of content kids can watch and block specific titles you don’t want kids to see.",
+            title:t('is_netflix_good_for_kids'),
+            desc:t('is_netflix_good_for_kids_answer'),
             id:6
         },
         {
-            title:"Why am I seeing this language?",
-            desc:"Your browser preferences determine the language shown here.",
+            title:t('why_seeing_this_language'),
+            desc:t('why_seeing_this_language_answer'),
             id:7
         },
     ]
@@ -45,7 +48,7 @@ const FAQ = () => {
     
     return (
         <div className='mt-[64px]'>
-            <h2 className='text-[24px] font-medium text-white mb-4'>Frequently Asked Questions</h2>
+            <h2 className='text-[24px] font-medium text-white mb-4'>{t('frequently_asked_questions')}</h2>
             <div className='flex flex-col gap-[8px]'>
                 {
                     items.map(item => <FAQItem item={item} openItem={openItem} setOpenItem={setOpenItem} />)
--- src/landing/components/ReasonsToJoin.jsx
+++ src/landing/components/ReasonsToJoin.jsx
@@ -4,33 +4,37 @@ import face from "../../assets/images/face.svg"
 import dowland from "../../assets/images/dowland.svg"
 import React from 'react' 
 import ReasonCard from "./ReasonCard"
+import { useTranslation } from 'react-i18next';
+
+
 const ReasonsToJoin = () => {
+    const { t } = useTranslation(); 
     const items = [
         {
-            title:"Enjoy on your TV",
-            desc:"Watch on Smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blu-ray players, and more.",
+            title:t('enjoy_on_your_tv'),
+            desc:t('watch_on_smart_tv'),
             img:televizon
         },
         {
-            title:"Download your shows to watch offline",
-            desc:"Save your favorites easily and always have something to watch.",
+            title:t('download_your_shows_to_watch_offline'),
+            desc:t('save_your_favorit'),
             img:dowland
         },
         {
-            title:"Watch everywhere",
-            desc:"Stream unlimited movies and TV shows on your phone, tablet, laptop, and TV.",
+            title:t('watch_everywhere'),
+            desc:t('stream_unlimited'),
             img:watch
         },
         {
-            title:"Create profiles for kids",
-            desc:"Send kids on adventures with their favorite characters in a space made just for them — free with your membership.",
+            title:t('create_profiles_for_kids'),
+            desc:t('send_kids'),
             img:face
         }
     ]
   
     return (
     <div>
-        <h2 className="text-white font-medium text-[24px] mb-4">More Reasons to Join</h2>
+        <h2 className="text-white font-medium text-[24px] mb-4">{t('more_reasons_to_join')}</h2>
         <div className="flex gap-4">
             {items.map(item => <ReasonCard item={item}/>)}
         </div>
